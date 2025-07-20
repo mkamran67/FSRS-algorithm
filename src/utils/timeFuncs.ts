0ee1066ddd9eff77f1b37dacb0cd08a7c0b2ebf9
mk <mkamran1967@gmail.com>
2025-07-19 23:04:27 -0700

    Fixed a date validation issue with timeFuncs util function.

--- src/utils/timeFuncs.ts
+++ src/utils/timeFuncs.ts
@@ -1,10 +1,19 @@
 export function calcElapsedDays(lastReviewDate: Date | null | undefined, now: Date): number {
 	if (lastReviewDate) {
-		return Math.max(
-			0,
-			Math.floor((now.getTime() - lastReviewDate.getTime()) / (1000 * 60 * 60 * 24))
-		);
+		if (isValidDate(new Date(lastReviewDate))) {
+			return Math.max(
+				0,
+				Math.floor((now.getTime() - lastReviewDate.getTime()) / (1000 * 60 * 60 * 24))
+			);
+		} else {
+			console.warn("Invalid date provided, make sure your dates are valid dates!");
+		}
 	}
 
 	return 0;
+}
+
+export function isValidDate(date: any): boolean {
+	// An invalid date object returns NaN for getTime()
+	return date instanceof Date && !isNaN(date.getTime());
 }
